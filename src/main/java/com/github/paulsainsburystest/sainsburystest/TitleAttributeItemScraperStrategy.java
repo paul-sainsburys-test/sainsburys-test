4f1231bfb086283bf7dc3960531b239f7e942274
Paul <35873628+paul-sainsburys-test@users.noreply.github.com>
2018-01-28 23:17:55 +0000

    Implement the extraction of a item's title in TitleAttributeItemScraperStrategy.

--- src/main/java/com/github/paulsainsburystest/sainsburystest/TitleAttributeItemScraperStrategy.java
+++ src/main/java/com/github/paulsainsburystest/sainsburystest/TitleAttributeItemScraperStrategy.java
@@ -1,6 +1,9 @@
 package com.github.paulsainsburystest.sainsburystest;
 
+
 import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
 
 /**
  * This strategy gets an item's title.
@@ -29,7 +32,30 @@ public class TitleAttributeItemScraperStrategy extends AbstractItemAttributeScra
   @Override
   protected String getAttributeInputNullChecked(Document jsoupDocument)
   {
-    throw new UnsupportedOperationException("Not supported yet.");
+    //Pick the (unique) element id closest to the data we want to extract.
+    Element contentElement = jsoupDocument.getElementById("content");
+
+    //It may be possible to go stright from this element to the last element tag,
+    //then to h1 but it's more robust to step down each node in the tree.
+    //Class tags would be more stable then the fine layout of the webpage too.
+
+    Elements productContents = contentElement.getElementsByClass("productContent");
+    Element productContent = productContents.first();
+
+    Elements pdps = productContent.getElementsByClass("pdp");
+    Element pdp = pdps.first();
+
+    Elements productSummaries = pdp.getElementsByClass("productSummary");
+    Element productSummary = productSummaries.first();
+
+    Elements productTitleDescriptionContainers = productSummary.getElementsByClass("productTitleDescriptionContainer");
+    Element productTitleDescriptionContainer = productTitleDescriptionContainers.first();
+
+    Elements h1s = productTitleDescriptionContainer.getElementsByTag("h1");
+    Element h1 = h1s.first();
+
+    return h1.text();
+
   }
 
 }
