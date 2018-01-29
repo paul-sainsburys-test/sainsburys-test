d6f8dabac827af9d63db37b3f14f851b486b6c9e
Paul <35873628+paul-sainsburys-test@users.noreply.github.com>
2018-01-29 02:45:59 +0000

    Implement the extraction of an item's kilocalories per 100g in KilocaloriesAttributeItemScraperStrategy.

--- src/main/java/com/github/paulsainsburystest/sainsburystest/KilocaloriesAttributeItemScraperStrategy.java
+++ src/main/java/com/github/paulsainsburystest/sainsburystest/KilocaloriesAttributeItemScraperStrategy.java
@@ -1,6 +1,10 @@
 package com.github.paulsainsburystest.sainsburystest;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
 
 /**
  * This strategy gets an item's kilocalories per 100g if it's available.
@@ -8,6 +12,13 @@ import org.jsoup.nodes.Document;
  */
 public class KilocaloriesAttributeItemScraperStrategy extends AbstractItemAttributeScraperStrategy<Integer>
 {
+  /**
+   * Pattern to extract the kcal value from.
+   * Match the whole string starting with a non-zero length sequence of numbers and
+   * optionally ending in "kcal".
+   */
+  public static final Pattern KCAL_PATTERN = Pattern.compile("^(\\d+)(kcal)?$");
+
   /** Are null attributes allowed to return from this class?. */
   public static final boolean ALLOWS_FOR_NULL_ATTRIBUTE = true;
 
@@ -29,7 +40,46 @@ public class KilocaloriesAttributeItemScraperStrategy extends AbstractItemAttrib
   @Override
   protected Integer getAttributeInputNullChecked(Document jsoupDocument)
   {
-    throw new UnsupportedOperationException("Not supported yet.");
+    //Pick the (unique) element id closest to the data we want to extract.
+    Element contentElement = jsoupDocument.getElementById("information");
+
+    //There is no other tags/classes to step down onto without iterating.
+    //This class seems to be uniquely identifying, so jump onto this one.
+    Elements nutritionTables = contentElement.getElementsByClass("nutritionTable");
+    Element nutritionTable = nutritionTables.first();
+
+    //Does it have a nutrition table?
+    if (nutritionTable == null)
+    {
+      return null;
+    }
+
+    //Skip the headers and go into the body.
+    Elements tableBodies = nutritionTable.getElementsByTag("tbody");
+    Element tableBody = tableBodies.first();
+
+    //Get the second table row.
+    Elements tableRows = tableBody.getElementsByTag("tr");
+    Element tableRow = tableRows.get(1);
+
+    //Select the table data, making sure to ignore table header tag ("th").
+    Elements tableDataMulti = tableRow.getElementsByTag("td");
+    Element tableData = tableDataMulti.first();
+    String data = tableData.text();
+
+    //Most are labeled as "<number>kcal" but one is just "<number>".
+    Matcher matcher = KCAL_PATTERN.matcher(data);
+    if (matcher.find())
+    {
+      String kcalStr = matcher.group(1); //Get the numbers
+      return Integer.parseInt(kcalStr);
+    }
+    else
+    {
+      throw new MalformedDocumentException("The data was found but was not in " +
+          "the correct pattern (\""+data+"\")");
+    }
   }
 
 }
+
