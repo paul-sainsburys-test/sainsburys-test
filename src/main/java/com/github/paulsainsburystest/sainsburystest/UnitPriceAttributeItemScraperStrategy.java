f41837bb3d2f1ef0cd2135277d308dfc9837874e
Paul <35873628+paul-sainsburys-test@users.noreply.github.com>
2018-01-29 11:35:03 +0000

    Implement the extraction of an item's price in UnitPriceAttributeItemScraperStrategy.

--- src/main/java/com/github/paulsainsburystest/sainsburystest/UnitPriceAttributeItemScraperStrategy.java
+++ src/main/java/com/github/paulsainsburystest/sainsburystest/UnitPriceAttributeItemScraperStrategy.java
@@ -1,7 +1,11 @@
 package com.github.paulsainsburystest.sainsburystest;
 
 import java.math.BigDecimal;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
 
 /**
  * This strategy gets an item's price.
@@ -12,6 +16,15 @@ import org.jsoup.nodes.Document;
  */
 public class UnitPriceAttributeItemScraperStrategy extends AbstractItemAttributeScraperStrategy<BigDecimal>
 {
+  /**
+   * Pattern to extract price from.
+   *
+   * Match the whole string starting with a "£", then a non-zero length sequence
+   * of numbers, a decimal point ".", two more digits and finishing off with "/unit".
+   *
+   * The main capture group captures the value after the "£" and before "/unit".
+   */
+  public static final Pattern PRICE_PER_UNIT_PATTERN = Pattern.compile("^£((\\d+)\\.(\\d{2}))/unit$");
 
   /** Are null attributes allowed to return from this class?. */
   public static final boolean ALLOWS_FOR_NULL_ATTRIBUTE = false;
@@ -34,7 +47,50 @@ public class UnitPriceAttributeItemScraperStrategy extends AbstractItemAttribute
   @Override
   protected BigDecimal getAttributeInputNullChecked(Document jsoupDocument)
   {
-    throw new UnsupportedOperationException("Not supported yet.");
+    //Pick the (unique) element id closest to the data we want to extract.
+    Element contentElement = jsoupDocument.getElementById("content");
+
+    //It may be possible to go stright from this element to class "pricePerUnit",
+    //but it's more robust to step down the tree.
+
+    Elements productContents = contentElement.getElementsByClass("productContent");
+    Element productContent = productContents.first();
+
+    Elements pdps = productContent.getElementsByClass("pdp");
+    Element pdp = pdps.first();
+
+    Elements productSummaries = pdp.getElementsByClass("productSummary");
+    Element productSummary = productSummaries.first();
+
+    Elements addToTrolleytabBoxes = productSummary.getElementsByClass("addToTrolleytabBox");
+    Element addToTrolleytabBox = addToTrolleytabBoxes.first();
+
+    Elements addToTrolleytabContainers = addToTrolleytabBox.getElementsByClass("addToTrolleytabContainer");
+    Element addToTrolleytabContainer = addToTrolleytabContainers.first();
+
+    Elements pricingAndTrolleyOptions = addToTrolleytabContainer.getElementsByClass("pricingAndTrolleyOptions");
+    Element pricingAndTrolleyOption = pricingAndTrolleyOptions.first();
+
+    Elements prices = pricingAndTrolleyOption.getElementsByClass("pricing");
+    Element pricing = prices.first();
+
+    Elements pricePerUnits = pricing.getElementsByClass("pricePerUnit");
+    Element pricePerUnit = pricePerUnits.first();
+
+    //Some sample text: "£0.50/unit" or "£1.00/unit"
+    String data = pricePerUnit.text();
+
+    Matcher matcher = PRICE_PER_UNIT_PATTERN.matcher(data);
+    if (matcher.find())
+    {
+      String priceString = matcher.group(1); //Get all of the numbers.
+      return new BigDecimal(priceString);
+    }
+    else
+    {
+      throw new MalformedDocumentException("The data was found but was not in " +
+          "the correct pattern (\""+data+"\")");
+    }
   }
 
 }
