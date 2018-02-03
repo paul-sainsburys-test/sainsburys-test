a3e7945e9b4af7de6e218fbf01e3bc48a7248ea7
Paul <35873628+paul-sainsburys-test@users.noreply.github.com>
2018-02-03 06:51:22 +0000

    Create a static globals class for the tests.
    Store a webpage cache instance in this.

---
+++ src/test/java/com/github/paulsainsburystest/sainsburystest/Globals.java
@@ -0,0 +1,12 @@
+package com.github.paulsainsburystest.sainsburystest;
+
+/**
+ * Static variables.
+ * @author Paul
+ */
+public class Globals
+{
+  /** A cache for webpages. */
+  public static SimpleWebpageCache webpageCache = new SimpleWebpageCache();
+
+}
