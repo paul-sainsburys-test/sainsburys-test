775242c61c42ad2df15462a885e2c3cf190cc7d3
Paul <35873628+paul-sainsburys-test@users.noreply.github.com>
2018-02-05 18:51:15 +0000

    Create the interface IItemScraperStrategy for defining classes which get an item url list from a category webpage.

---
+++ src/main/java/com/github/paulsainsburystest/sainsburystest/itemscraperstrategies/IItemScraperStrategy.java
@@ -0,0 +1,45 @@
+package com.github.paulsainsburystest.sainsburystest.itemscraperstrategies;
+
+import com.github.paulsainsburystest.sainsburystest.MalformedDocumentException;
+import java.util.List;
+import org.jsoup.nodes.Document;
+
+/**
+ * The interface for defining a strategy to extract item urls from a category webpage.
+ * @author Paul
+ */
+public interface IItemScraperStrategy
+{
+  /**
+   * Given a html document (which is assumed to be a category page) extract all of
+   * urls of the items listed.
+   * @param jsoupDocument The document to extract from.
+   * @return A list of all items on the page (not null).
+   * @throws NullPointerException If jsoupDocument is null.
+   * @throws MalformedDocumentException If the strategy cannot determine if the
+   *    items should exist.
+   */
+  public List<String> getItemUrls(Document jsoupDocument)
+      throws NullPointerException, MalformedDocumentException;
+
+  /**
+   * The name which is used to identify this scraper strategy.
+   * @return A non-null string.
+   */
+  public String getName();
+
+  /**
+   * Determines if an instance is the same.
+   * @param obj Object to compare with.
+   * @return Equals with {@link #getName() }.
+   */
+  @Override
+  public boolean equals(Object obj);
+
+  /**
+   * Calculates the hashcode of the instance.
+   * @return Hashcode of {@link #getName() }.
+   */
+  @Override
+  public int hashCode();
+}
