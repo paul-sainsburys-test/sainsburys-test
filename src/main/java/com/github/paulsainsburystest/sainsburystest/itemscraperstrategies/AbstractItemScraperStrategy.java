7b879615933019b4b53586870cea05145667573b
Paul <35873628+paul-sainsburys-test@users.noreply.github.com>
2018-02-05 22:44:19 +0000

    Implement the skeleton of the AbstractItemScraperStrategy class.

---
+++ src/main/java/com/github/paulsainsburystest/sainsburystest/itemscraperstrategies/AbstractItemScraperStrategy.java
@@ -0,0 +1,50 @@
+package com.github.paulsainsburystest.sainsburystest.itemscraperstrategies;
+
+import com.github.paulsainsburystest.sainsburystest.MalformedDocumentException;
+import java.util.List;
+import org.jsoup.nodes.Document;
+
+/**
+ * Abstract base class that can be used to ease the implementation of
+ * {@link IItemScraperStrategy}.
+ *
+ * @author Paul
+ */
+public abstract class AbstractItemScraperStrategy implements IItemScraperStrategy
+{
+
+  @Override
+  public List<String> getItemUrls(Document jsoupDocument)
+      throws NullPointerException, MalformedDocumentException
+  {
+    throw new UnsupportedOperationException("Not supported yet.");
+  }
+
+  /**
+   * Given a html {@link Document} (which is assumed to be a category page) extract
+   * all of urls of the items listed.
+   *
+   * This method parameter will always be not null.
+   * @param jsoupDocument The document to extract from (always will be non-null).
+   * @return A list of all items on the page (not null).
+   * @throws NullPointerException If jsoupDocument is null.
+   * @throws MalformedDocumentException If the strategy cannot determine if the
+   *    items should exist.
+   */
+  protected abstract List<String> getItemUrlsInputNullChecked(Document jsoupDocument)
+      throws NullPointerException, MalformedDocumentException;
+
+
+  @Override
+  public boolean equals(Object obj)
+  {
+    return super.equals(obj);
+  }
+
+  @Override
+  public int hashCode()
+  {
+    return super.hashCode();
+  }
+
+}
