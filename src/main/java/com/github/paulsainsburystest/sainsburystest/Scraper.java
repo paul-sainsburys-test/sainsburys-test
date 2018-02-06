785a0d65f3db79cfcf7e5bc06cdfcfa9fe1f5d30
Paul <35873628+paul-sainsburys-test@users.noreply.github.com>
2018-02-06 22:58:57 +0000

    Add a skeleton implementation of the class Scraper.

---
+++ src/main/java/com/github/paulsainsburystest/sainsburystest/Scraper.java
@@ -0,0 +1,54 @@
+package com.github.paulsainsburystest.sainsburystest;
+
+import com.github.paulsainsburystest.sainsburystest.itemattributescraperstrategies.IItemAttributeScraperStrategy;
+import com.github.paulsainsburystest.sainsburystest.itemscraperstrategies.IItemScraperStrategy;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+/**
+ * This class scrapes an item's page
+ * @author Paul
+ */
+public class Scraper
+{
+  /** Strategies for scraping attributes. */
+  private final Set<IItemAttributeScraperStrategy<?>> itemAttributeScraperStrategies;
+
+  /** Strategy for scraping item pages. */
+  private final IItemScraperStrategy itemScraperStrategies;
+
+  /**
+   * The constructor for this class.
+   * @param itemScraperStrategy The strategy of how items will be scraped/selected.
+   * @param itemAttributeScraperStrategies The set of strategies for scraping attributes.
+   * @throws NullPointerException If the parameter is null.
+   */
+  public Scraper(IItemScraperStrategy itemScraperStrategy,
+      Set<IItemAttributeScraperStrategy<?>> itemAttributeScraperStrategies)
+  {
+    throw new UnsupportedOperationException("Not implemented yet.");
+  }
+
+  /**
+   * Scrape a category for all item attributes contained within it.
+   * @param categoryUrl Url of a category to parse.
+   * @return A list of mapped attributes.
+   */
+  public List<Map<String,Object>> scrape(String categoryUrl)
+  {
+    throw new UnsupportedOperationException("Not implemented yet.");
+  }
+
+  /**
+   * Scrape an item's page for attributes.
+   * @param url The url to scrape from.
+   * @return A hash map of attribute names and their associated attribute.
+   */
+  public Map<String, Object> scrapeItemPage(String url)
+  {
+    throw new UnsupportedOperationException("Not implemented yet.");
+  }
+
+}
+
