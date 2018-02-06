985377c471148a33359f3a4a0e99f55acd92173c
Paul <35873628+paul-sainsburys-test@users.noreply.github.com>
2018-02-06 23:15:44 +0000

    Add tests for the Scraper class constructor.

---
+++ src/test/java/com/github/paulsainsburystest/sainsburystest/ScraperTest.java
@@ -0,0 +1,62 @@
+package com.github.paulsainsburystest.sainsburystest;
+
+import com.github.paulsainsburystest.sainsburystest.itemattributescraperstrategies.IItemAttributeScraperStrategy;
+import com.github.paulsainsburystest.sainsburystest.itemscraperstrategies.IItemScraperStrategy;
+import com.github.paulsainsburystest.sainsburystest.itemscraperstrategies.SinglePageItemScraperStrategy;
+import java.util.HashSet;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
+
+/**
+ * Test for the class {@link Scraper}.
+ * @author Paul
+ */
+public class ScraperTest
+{
+  @Rule
+  public final ExpectedException expectedException = ExpectedException.none();
+
+  /**
+   * Test when the {@link IItemAttributeScraperStrategy} parameter is null.
+   * @throws NullPointerException Always thrown.
+   */
+  @Test
+  public void scraperConstructorItemScraperStrategyNullTest()
+  {
+    //An empty set is valid
+    HashSet<IItemAttributeScraperStrategy<?>> attributeScraperStrategySet = new HashSet<>();
+
+    this.expectedException.expect(NullPointerException.class);
+    this.expectedException.expectMessage("itemScraperStrategy cannot be null");
+    Scraper scraper = new Scraper(null, attributeScraperStrategySet);
+  }
+
+  /**
+   * Test when the {@link IItemScraperStrategy} parameter is null.
+   * @throws NullPointerException Always thrown.
+   */
+  @Test
+  public void scraperConstructorItemAttributeScraperStrategiesNullTest()
+  {
+    IItemScraperStrategy scraperStrategy = new SinglePageItemScraperStrategy();
+
+    this.expectedException.expect(NullPointerException.class);
+    this.expectedException.expectMessage("itemScraperStrategy cannot be null");
+    Scraper scraper = new Scraper(scraperStrategy, null);
+  }
+
+  /**
+   * Test when both parameters are null.
+   * @throws NullPointerException Always thrown.
+   */
+  @Test
+  public void scraperConstructorNullNullTest()
+  {
+    this.expectedException.expect(NullPointerException.class);
+    //We can't predict the message, it'll be either of the above.
+    Scraper scraper = new Scraper(null, null);
+  }
+
+
+}
