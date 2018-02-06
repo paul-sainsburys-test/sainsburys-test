dfac88d3c0e93e8b7ce5d44e4f06205d3ac19144
Paul <35873628+paul-sainsburys-test@users.noreply.github.com>
2018-02-06 07:02:57 +0000

    Implement SinglePageItemScraperStrategy to get all of the item urls from a category.

--- src/main/java/com/github/paulsainsburystest/sainsburystest/itemscraperstrategies/SinglePageItemScraperStrategy.java
+++ src/main/java/com/github/paulsainsburystest/sainsburystest/itemscraperstrategies/SinglePageItemScraperStrategy.java
@@ -1,8 +1,11 @@
 package com.github.paulsainsburystest.sainsburystest.itemscraperstrategies;
 
 import com.github.paulsainsburystest.sainsburystest.MalformedDocumentException;
+import java.util.LinkedList;
 import java.util.List;
 import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
 
 /**
  * The strategy to extract a list of items from a category page.
@@ -22,6 +25,31 @@ public class SinglePageItemScraperStrategy extends AbstractItemScraperStrategy
   protected List<String> getItemUrlsInputNullChecked(Document jsoupDocument)
       throws NullPointerException, MalformedDocumentException
   {
-    throw new UnsupportedOperationException("Not supported yet.");
+    Element productListerId = jsoupDocument.getElementById("productLister");
+
+    Element productListerClass = productListerId.getElementsByClass("productLister").first();
+
+    //All of the items.
+    Elements items = productListerClass.getElementsByClass("gridItem");
+
+    LinkedList<String> list = new LinkedList<>();
+    for (Element item : items)
+    {
+      //Avoid cross sell.
+      Element productInfo = item.getElementsByClass("productInfo").first();
+
+      //Avoid offer links
+      Element h3 = productInfo.getElementsByTag("h3").first();
+
+      //Get the link.
+      Element a = h3.getElementsByTag("a").first();
+
+      //Get absolute reference.
+      String url = a.absUrl("href");
+      list.add(url);
+    }
+
+    return list;
+
   }
 }
