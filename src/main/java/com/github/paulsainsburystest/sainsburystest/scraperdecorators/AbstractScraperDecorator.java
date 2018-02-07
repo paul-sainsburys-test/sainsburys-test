35608674acb6f06c129ade20fd53a9277f869cb6
Paul <35873628+paul-sainsburys-test@users.noreply.github.com>
2018-02-07 17:57:42 +0000

    Add a skeleton implementation of the class AbstractScraperDecorator.

---
+++ src/main/java/com/github/paulsainsburystest/sainsburystest/scraperdecorators/AbstractScraperDecorator.java
@@ -0,0 +1,22 @@
+package com.github.paulsainsburystest.sainsburystest.scraperdecorators;
+
+/**
+ * This is the base class for the {@link IScraperDecorator} interface implementation.
+ * @author Paul
+ */
+public abstract class AbstractScraperDecorator implements IScraperDecorator
+{
+  /** The next decorator in the chain (non-null). */
+  protected final IScraperDecorator nextScraperDecorator;
+
+  /**
+   * The constructor for this class.
+   * @param nextScraperDecorator The next scraper decorator to be called in this chain.
+   * @throws NullPointerException If the parameter is null.
+   */
+  public AbstractScraperDecorator(IScraperDecorator nextScraperDecorator)
+  {
+    throw new UnsupportedOperationException("This is not implemented yet.");
+  }
+
+}
